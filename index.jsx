0654a32bda42f615e99612294e91462ba5833ce1
AlePouroullis <alexpouroullis123@gmail.com>
2022-04-27 14:47:11 +0200

    not sure what I changed lol

--- index.jsx
+++ index.jsx
@@ -4,13 +4,20 @@ import './style.css';
 
 const root = document.getElementById('root');
 
+function LoginVerdict(props){
+    console.log(typeof(props.inputPassword));
+    if(props.inputUsername === props.username && props.inputPassword === props.password){
+        return <p>Successfully logged in</p>;
+    }
+    return <p>The username or password you entered is incorrect.</p>;
+}
+
 class LoginForm extends React.Component {
     constructor(props){
         super(props);
         this.state = {
             'usernameValue': '',
             'passwordValue': '',
-            'selection': '1'
         }
         
     }
@@ -27,27 +34,22 @@ class LoginForm extends React.Component {
         })
     }
 
-    handleSelection(event){
-        this.setState({
-            selection: event.target.value
-        });
-    }
-
     login(){
         alert("Your username is " + this.state.usernameValue + " and your password is " + this.state.passwordValue);
     }
-
+    
     render() {
+        const username = 'Alexandros';
+        const password = '123';
         return (
             <form id="login-form">
-                <input type="text" id="username-button" value={this.state.usernameValue} onChange={(event) => this.handleUsernameChange(event)}></input>
+                <input type="text" id="username-field" value={this.state.usernameValue} onChange={(event) => this.handleUsernameChange(event)}></input>
                 <input type="text" id="password-field" value={this.state.passwordValue} onChange={(event) => this.handlePasswordChange(event)}></input>
                 <button  id="login-button" onClick={() => this.login()}>Login</button>
-                <select value={this.state.selection} onChange={(event) => this.handleSelection(event)}>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                </select>
+                <LoginVerdict username={username}
+                    inputUsername={this.state.usernameValue} 
+                    password={password}
+                    inputPassword={this.state.passwordValue} />
             </form>
         );
     }
